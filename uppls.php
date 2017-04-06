aa7cf07dcaf76920aa249fecc60cc0a2480b7b10
hákon klaus <hkon klaus>
2017-04-06 15:06:37 +0000

    prufa

---
+++ uppls.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="hopaverkefni">
+	<title>blómabúðin okkar</title>
+	<link rel="stylesheet" type="text/css" href="layout.css">
+	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Indie+Flower" />
+</head>
+<body>
+<?php require  './includes/nav.php'; ?>
+
+<div class="kynna">
+	<h1>Upplýsingar um okkur síðu</h1>
+	<h4>Þessi vefsíða er hópaverkefni hjá okkur hákon og Yngva.</h4>
+	<h3>Þessi síða er vefverslun þar sem þú getur keypt blóm og fræ</h3>
+	<h3>Þú getur filst með öllum breytingum á þessari síðu há</h3>
+	<button>
+	<a href="https://github.com/DonNinja/lokaverkefnivsh2b">github.com</a>
+	</button>
+	<h3>eða með því að koma á þessa síðu reglulega</h3>
+</div>
+<div class="location">
+	<h1>Staðsettning fyrirtækis</h1>
+	<h3>Okkur getur þú fundið á egilsgötu 10 í upplýsingatækniskólan</h3>
+</div>
+<div class="location">
+	<img src="img/tskoli.JPG">
+</div>
+
+<?php require './includes/footer.php'; ?>
+</body>
+</html>
